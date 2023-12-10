7119084175fc3a28cc73cd8f607d6c03cda3a72b
PrateekSavanur <prateek.savanur@gmail.com>
2023-12-10 10:43:49 +0530

    Till express

--- server.js
+++ server.js
@@ -1,10 +1,51 @@
 const dotenv = require("dotenv")
+const mongoose = require("mongoose")
+const app = require("./app")
 
 dotenv.config({ path: "./config.env" })
 
-const app = require("./app")
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD,
+)
 
-console.log(process.env.NODE_ENV)
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: true, // Just to avoid deprecation warnings
+  })
+  .then(() => {
+    console.log("Database connection successful")
+  })
+
+const toursSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Tour must have a name"],
+    // unique: true,
+  },
+  rating: { type: Number, default: 4.5 },
+  price: { type: Number, required: [true, "Tour must have a price"] },
+})
+// created a model
+const Tour = mongoose.model("Tour", toursSchema)
+
+const testTour = new Tour({
+  name: "The Snow Hiker",
+  rating: 4.9,
+  price: 497,
+})
+
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc)
+  })
+  .catch((err) => {
+    console.log("Error ", err)
+  })
 
 const port = process.env.PORT || 8000
 app.listen(port, () => {
